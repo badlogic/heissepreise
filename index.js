38bcb53bc82741b7f7cb2f758e813717fd298da9
Mario Zechner <badlogicgames@gmail.com>
2023-05-15 15:06:30 +0200

    Store price history.

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ function currentDate() {
 }
 
 async function updateData() {
-    console.log("Fetching data");
+    const today = currentDate();
+
+    console.log("Fetching data for " + today);
 
     const sparItems = (await axios.get(SPAR_SEARCH)).data.hits;
-    fs.writeFileSync(`data/spar-${currentDate()}.json`, JSON.stringify(sparItems, null, 2));
+    fs.writeFileSync(`data/spar-${today}.json`, JSON.stringify(sparItems, null, 2));
     const sparItemsCanonical = [];
     for (let i = 0; i < sparItems.length; i++) {
         const item = sparItems[i];
@@ -25,13 +27,14 @@ async function updateData() {
             store: "spar",
             name: item.masterValues.title + " " + item.masterValues["short-description"],
             price: item.masterValues.price,
+            priceHistory: [{date: today, price: item.masterValues.price}],
             unit: item.masterValues["short-description-3"]            
         });
     }
-    fs.writeFileSync(`data/spar-${currentDate()}-canonical.json`, JSON.stringify(sparItemsCanonical, null, 2));
+    fs.writeFileSync(`data/spar-${today}-canonical.json`, JSON.stringify(sparItemsCanonical, null, 2));
 
     const billaItems = (await axios.get(BILLA_SEARCH)).data.tiles;
-    fs.writeFileSync(`data/billa-${currentDate()}.json`, JSON.stringify(billaItems, null, 2));
+    fs.writeFileSync(`data/billa-${today}.json`, JSON.stringify(billaItems, null, 2));
     const billaItemsCanonical = [];
     for (let i = 0; i < billaItems.length; i++) {
         const item = billaItems[i];
@@ -39,19 +42,43 @@ async function updateData() {
             store: "billa",
             name: item.data.name,
             price: item.data.price.final,
+            priceHistory: [{date: today, price: item.data.price.final}],
             unit: item.data.grammage        
         });
     }
-    fs.writeFileSync(`data/billa-${currentDate()}-canonical.json`, JSON.stringify(billaItemsCanonical, null, 2));
+    fs.writeFileSync(`data/billa-${today}-canonical.json`, JSON.stringify(billaItemsCanonical, null, 2));
 
-    fs.writeFileSync(`data/latest-canonical.json`, JSON.stringify([...billaItemsCanonical, ...sparItemsCanonical], null, 2));
+    const allItems = [...billaItemsCanonical, ...sparItemsCanonical];
+    if (fs.existsSync("data/latest-canonical.json")) {
+        const oldItems = JSON.parse(fs.readFileSync("data/latest-canonical.json"));    
+        const lookup = {}
+        for (oldItem of oldItems) {
+            lookup[oldItem.name] = oldItem;
+        }
+
+        for (item of allItems) {
+            let oldItem = lookup[item.name];
+            let currPrice = item.priceHistory[0];
+            if (oldItem) {
+                for (oldPrice of oldItem.priceHistory) {
+                    if (oldPrice.date != currPrice.date)
+                        item.priceHistory.push(oldPrice);
+                }
+            }
+        }
+    }
+
+    fs.writeFileSync(`data/latest-canonical.json`,  JSON.stringify(allItems, null, 2));
 
     console.log("Updated data");
+    items = allItems;
 }
+
+let items = null;
 
 (async () => {
     await updateData();    
-    let items = JSON.parse(fs.readFileSync("data/latest-canonical.json"));    
+    setInterval(updateData, 1000 * 60 * 60 * 24);
     
     const express = require('express')
     const compression = require('compression');
