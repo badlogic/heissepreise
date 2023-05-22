ff569df7fb21cf455d76050e46f1f01606125342
Mario Zechner <badlogicgames@gmail.com>
2023-05-22 13:54:57 +0200

    Restore updates data.

--- restore.js
+++ restore.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const analysis = require("./analysis.js");
 
-fs.copyFileSync("docker/data/latest-canonical.json", "docker/data/latest-canonical-old.json");
-fs.writeFileSync("docker/data/latest-canonical.json", JSON.stringify(analysis.replay("docker/data"), null, 2));
+(async function () {
+    fs.copyFileSync("docker/data/latest-canonical.json", "docker/data/latest-canonical-old.json");
+    await analysis.updateData("docker/data");
+    fs.writeFileSync("docker/data/latest-canonical.json", JSON.stringify(analysis.replay("docker/data"), null, 2));
+})();
