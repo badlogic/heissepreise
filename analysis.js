fadb104d7282e96cec8774fe918785691f3c977c
Simeon Macke (01505675) <simmac@users.noreply.github.com>
2023-05-25 12:28:12 +0200

    Add DM support

--- analysis.js
+++ analysis.js
@@ -79,6 +79,24 @@ function hoferToCanonical(rawItems, today) {
     return canonicalItems;
 }
 
+function dmToCanonical(rawItems, today) {
+    const canonicalItems = [];
+    for (let i = 0; i < rawItems.length; i++) {
+        const item = rawItems[i];
+        canonicalItems.push({
+            store: "dm",
+            id: item.gtin,
+            name: `${item.brandName} ${item.title}`,
+            price: item.price.value,
+            priceHistory: [{ date: today, price: item.price.value }],
+            unit: `${item.netQuantityContent} ${item.contentUnit}`,
+            ...(item.brandName === "dmBio" || (item.name ? (item.name.startsWith("Bio ") | item.name.startsWith("Bio-")) : false)) && {bio: true},
+        });
+    }
+
+    return canonicalItems;
+}
+
 async function fetchHofer() {
     const BASE_URL = `https://shopservice.roksh.at`
     const CATEGORIES = BASE_URL + `/category/GetFullCategoryList/`
@@ -106,6 +124,67 @@ async function fetchHofer() {
     }
 
     return hoferItems;
+}
+
+async function fetchDm() {
+    // The DM API is pretty aggressive regarding rate limiting, also, every query returns at most 1000 items,
+    // so we have to split the queries into multiple ones.
+    const BASE_URL = `https://product-search.services.dmtech.com/at/search/crawl?pageSize=1000&`
+    const QUERIES = [
+        'allCategories.id=010000&price.value.to=2', //~500 items
+        'allCategories.id=010000&price.value.from=2&price.value.to=3', //~600 items
+        'allCategories.id=010000&price.value.from=3&price.value.to=4', //~500 items
+        'allCategories.id=010000&price.value.from=4&price.value.to=7', //~800 items
+        'allCategories.id=010000&price.value.from=7&price.value.to=10', //~900 items
+        'allCategories.id=010000&price.value.from=10&price.value.to=15', //~900 items
+        'allCategories.id=010000&price.value.from=15', //~300 items
+        'allCategories.id=020000&price.value.to=2', //~600 items
+        'allCategories.id=020000&price.value.from=2&price.value.to=3', //~550 items
+        'allCategories.id=020000&price.value.from=3&price.value.to=4', //~600 items
+        'allCategories.id=020000&price.value.from=4&price.value.to=6', //~800 items
+        'allCategories.id=020000&price.value.from=6&price.value.to=10', //~850 items
+        'allCategories.id=020000&price.value.from=10&price.value.to=18', //~900 items
+        'allCategories.id=020000&price.value.from=18', //~960 items (!)
+        'allCategories.id=030000&price.value.to=8', //~900 items
+        'allCategories.id=030000&price.value.from=8', //~500 items
+        'allCategories.id=040000&price.value.to=2', //~600 items
+        'allCategories.id=040000&price.value.from=2&price.value.to=4', //~900 items
+        'allCategories.id=040000&price.value.from=4', //~400 items
+        'allCategories.id=050000&price.value.to=4', //~600 items
+        'allCategories.id=050000&price.value.from=4', //~800 items
+        'allCategories.id=060000&price.value.to=4', //~900 items
+        'allCategories.id=060000&price.value.from=4', //~500 items
+        'allCategories.id=070000', //~300 items
+    ]
+
+    let dmItems = [];
+    for (let query of QUERIES) {
+        var res = (await axios.get(BASE_URL + query, {
+            validateStatus: function (status) {
+                return (status >= 200 && status < 300) || status == 429;
+            }
+        }));
+
+        // exponential backoff
+        backoff = 2000;
+        while (res.status == 429) {
+            console.info(`DM API returned 429, retrying in ${backoff/1000}s.`);
+            await new Promise(resolve => setTimeout(resolve, backoff));
+            backoff *= 2;
+            res = (await axios.get(BASE_URL + query, {
+                validateStatus: function (status) {
+                    return (status >= 200 && status < 300) || status == 429;
+                }
+            }));
+        }
+        let items = res.data;
+        if (items.count > 1000) {
+            console.warn(`Query returned more than 1000 items! Items may be missing. Adjust queries. Query: ${query}`);
+        }
+        dmItems = dmItems.concat(items.products);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    return dmItems;
 }
 
 function mergePriceHistory(oldItems, items) {
@@ -154,12 +233,15 @@ exports.replay = function(rawDataDir) {
     const billaFilesCanonical = billaFiles.map(file => billaToCanonical(readJSON(file), file.match(/\d{4}-\d{2}-\d{2}/)[0]));
     const hoferFiles = files.filter(file => file.indexOf("hofer-") == 0).sort(dateSort).map(file => rawDataDir + "/" + file);
     const hoferFilesCanonical = hoferFiles.map(file => hoferToCanonical(readJSON(file), file.match(/\d{4}-\d{2}-\d{2}/)[0]));
+    const dmFiles = files.filter(file => file.indexOf("dm-") == 0).sort(dateSort).map(file => rawDataDir + "/" + file);
+    const dmFilesCanonical = dmFiles.map(file => dmToCanonical(readJSON(file), file.match(/\d{4}-\d{2}-\d{2}/)[0]));
 
     const allFilesCanonical = [];
-    const len = Math.max(sparFilesCanonical.length, Math.max(billaFilesCanonical.length, hoferFilesCanonical.length));
+    const len = Math.max(Math.max(sparFilesCanonical.length, Math.max(billaFilesCanonical.length, hoferFilesCanonical.length)), dmFilesCanonical.length);
     sparFilesCanonical.reverse();
     billaFilesCanonical.reverse();
     hoferFilesCanonical.reverse();
+    dmFilesCanonical.reverse();
     for (let i = 0; i < len; i++) {
         const canonical = [];
         let billa = billaFilesCanonical.pop();
@@ -168,6 +250,9 @@ exports.replay = function(rawDataDir) {
         if (spar) canonical.push(...spar);
         let hofer = hoferFilesCanonical.pop();
         if (hofer) canonical.push(...hofer);
+        allFilesCanonical.push(canonical);
+        let dm = dmFilesCanonical.pop();
+        if (dm) canonical.push(...dmFilesCanonical.pop());
         allFilesCanonical.push(canonical);
     }
 
@@ -210,7 +295,13 @@ exports.updateData = async function (dataDir, done) {
     const hoferItemsCanonical = hoferToCanonical(hoferItems, today);
     console.log("Fetched HOFER data, took " + (performance.now() - start) / 1000 + " seconds");
 
-    const items = [...billaItemsCanonical, ...sparItemsCanonical, ...hoferItemsCanonical];
+    start = performance.now();
+    const dmItems = await fetchDm();
+    fs.writeFileSync(`${dataDir}/dm-${today}.json`, JSON.stringify(dmItems, null, 2));
+    const dmItemsCanonical = dmToCanonical(dmItems, today);
+    console.log("Fetched DM data, took " + (performance.now() - start) / 1000 + " seconds");
+
+    const items = [...billaItemsCanonical, ...sparItemsCanonical, ...hoferItemsCanonical, ...dmItemsCanonical];
     if (fs.existsSync(`${dataDir}/latest-canonical.json`)) {
         const oldItems = JSON.parse(fs.readFileSync(`${dataDir}/latest-canonical.json`));
         mergePriceHistory(oldItems, items);
