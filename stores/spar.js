22f380c3907b9acdd6296026762bda4ba6e1b930
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-05-27 19:49:23 +0200

    Generate quantity and unified unit for spar data

--- stores/spar.js
+++ stores/spar.js
@@ -1,16 +1,46 @@
 const axios = require("axios");
 const HITS = Math.floor(30000 + Math.random() * 2000);
 
+const conversions = {
+    'G': { unit: 'g', factor: 1 },
+    'KG': { unit: 'g', factor: 1000 },
+    'L': { unit: 'ml', factor: 1000 },
+    'ML':   { unit: 'ml', factor: 1 },
+    'STK': { unit: 'stk', factor: 1 },
+    'Stück': { unit: 'stk', factor: 1 },
+    'kg': { unit: 'g', factor: 1000 },
+    'l': { unit: 'ml', factor: 1000 },
+    '100ml': { unit: 'ml', factor: 100 },
+    'WG': { unit: 'wg', factor: 1 },
+    '100g': { unit: 'g', factor: 100 },
+    'm': { unit: 'cm', factor: 100 },
+    'ml': { unit: 'ml', factor: 1 },
+};
+
 exports.getCanonical = function(item, today) {
-    let price, unit;
+    let price, unit, quantity;
     if (item.masterValues["quantity-selector"]) {
         const [str_price, str_unit] = item.masterValues["price-per-unit"].split('/');
         price = parseFloat(str_price.replace("€", ""));
-        unit = str_unit.trim();
     }
     else {
         price = item.masterValues.price;
-        unit = item.masterValues["short-description-3"];
+    }
+    if("short-description-3" in item.masterValues) {
+        let [rawQuantity, rawUnit] = item.masterValues["short-description-3"].replace(" EINWEG", "").replace(" MEHRWEG", "").trim().split(' ');
+        const conv = conversions[rawUnit];
+        quantity = parseFloat(rawQuantity.replace(',','.')) * conv.factor;
+        unit = conv.unit;
+    }
+    else{
+      // use price per unit to calculate quantity (less accurate)
+      let [unitPrice, rawUnit] = item.masterValues['price-per-unit'].split('/');
+      unitPrice = parseFloat(unitPrice.replace("€", ""));
+      const conv = conversions[rawUnit];
+      if(conv === undefined)
+          console.error("unknown unit: " + rawUnit)
+      quantity = Math.round(price / unitPrice * conv.factor)
+      unit = conv.unit;
     }
     return {
         id: item.masterValues["code-internal"],
@@ -19,6 +49,7 @@ exports.getCanonical = function(item, today) {
         price,
         priceHistory: [{ date: today, price }],
         unit,
+        quantity,
         bio: item.masterValues.biolevel === "Bio"
     };
 }
