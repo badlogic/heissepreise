a42ad8738c8f9073ac92ae68067fde72d59149e2
Mario Zechner <badlogicgames@gmail.com>
2023-05-28 11:26:17 +0200

    New workflow attempt.

--- pages.sh
+++ pages.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-set -e
-
-if [ -z "$1" ]; then
-  echo "Please provide a GitHub Pages repository URL, e.g. https://github.com/badlogic/badlogic.github.io"
-  exit 1
+repository_name=${GITHUB_REPOSITORY#*/}
+if [[ $repository_name == *.github.io ]]; then
+  echo "Name ends with github.io"
+else
+  echo "Name does not end with github.io, not generating pages"
+  exit
 fi
 
-repoUrl=$1
-pagesUrl="https://${repoUrl##*/}"
-
-rm -rf pages
 rm -rf tmp-data
 mkdir tmp-data
+mkdir -p docs
+cp docs/latest-canonical.json tmp-data
 
 npm install
-node pages.js tmp-data $pagesUrl
+node pages.js tmp-data
+cp tmp-data/latest-canonical.json docs
 
-git clone $repoUrl pages
-cp site/* pages
-cp tmp-data/latest-canonical.json pages
+cp site/* docs
 pushd pages
 git add *
 git commit -am "Updated $(date +'%Y-%m-%d')"
@@ -26,4 +24,3 @@ git push
 popd
 
 rm -rf tmp-data
-rm -rf pages
