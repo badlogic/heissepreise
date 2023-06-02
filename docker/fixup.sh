1e934e13ddb9ffe52bd88a84548b205662d00eed
Mario Zechner <badlogicgames@gmail.com>
2023-06-03 00:56:45 +0200

    Fix fixup.sh...

--- docker/fixup.sh
+++ docker/fixup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-pushd $dir > /dev/null
+pushd $dir/.. > /dev/null
 pushd site
 find . -type f -name "*.html" -print0 | while IFS= read -r -d $'\0' file; do
     sed -i 's/href="index\.html"/href="index-old.html"/g' "$file"
