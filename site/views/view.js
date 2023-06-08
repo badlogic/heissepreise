e35d30add7d98f7e0f1d9ae1304887372b18913c
Mario Zechner <badlogicgames@gmail.com>
2023-06-09 01:24:51 +0200

    Use cross-env so the scripts in package.json work on Windows as well.

--- site/views/view.js
+++ site/views/view.js
@@ -2,14 +2,14 @@ class View extends HTMLElement {
     constructor() {
         super();
         this._model = null;
-        this._listener = () => this.render(this._model);
+        this._listener = (model) => this.render(model);
     }
 
     set model(model) {
         if (this._model) this._model.removeListener(this._listener);
         this._model = model;
         this._model.addListener(this._listener);
-        this.render(this._model);
+        this.render(model);
     }
 
     render(model) {}
