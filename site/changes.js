529cb61204798ccf3a3766e6444c19f069071e92
Mario Zechner <badlogicgames@gmail.com>
2023-06-12 11:48:47 +0200

    Query sharing in changes.js.

--- site/changes.js
+++ site/changes.js
@@ -1,3 +1,4 @@
+const { getQueryParameter } = require("./misc");
 const model = require("./model");
 require("./views");
 
@@ -5,7 +6,35 @@ require("./views");
     await model.load();
     const itemsFilter = document.querySelector("items-filter");
     const itemsList = document.querySelector("items-list");
-    itemsList.elements.sort.value = "chain-and-name";
+    const itemsChart = document.querySelector("items-chart");
     itemsFilter.model = itemsList.model = model.items;
+
+    const stateToUrl = (event) => {
+        const filterState = itemsFilter.shareableState;
+        const listState = itemsList.shareableState;
+        const chartState = itemsChart.shareableState;
+        const chartedItems = model.items.filteredItems
+            .filter((item) => item.chart)
+            .map((item) => item.store + item.id)
+            .join(";");
+        history.pushState({}, null, location.pathname + "?f=" + filterState + "&l=" + listState + "&c=" + chartState + "&d=" + chartedItems);
+    };
+
+    itemsFilter.addEventListener("x-change", stateToUrl);
+    itemsList.addEventListener("x-change", stateToUrl);
+
+    const f = getQueryParameter("f");
+    const l = getQueryParameter("l");
+    const c = getQueryParameter("c");
+    const d = getQueryParameter("d");
+    if (f) itemsFilter.shareableState = f;
+    if (l) itemsList.shareableState = l;
+    if (c) itemsChart.shareableState = c;
+    if (d) {
+        for (const id of d.split(";")) {
+            model.items.lookup[id].chart = true;
+        }
+        itemsFilter.fireChangeEvent();
+    }
     itemsFilter.filter();
 })();
